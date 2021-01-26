032bf519814bc814cf6357e71c984a96101d7f93
anastasiia-titarenko1 <titnastya92@gmail.com>
2021-01-26 10:30:01 +0200

    HW 03

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,34 @@
 public class Student {
   int rating;
   private String name;
+  public static int count = 0;
+  public static int sumOfRating = 0;
 
   // TODO implement Student class according to the instructions provided in the README.md file
 
   public Student(String name) {
     //TODO initialize name
+    this.name = name;
+    count++;
+  }
+
+  private Student() {
+    count++;
+  }
+
+  private Student(String name, int rating) {
+    this.name = name;
+    this.rating = rating;
+    count++;
   }
 
   public static double getAvgRating() {
     // TODO return average rating of all students
-    return 0;
+    double avg = 0.0;
+    if (count > 0) {
+      avg = (double) sumOfRating / count;
+    }
+    return avg;
   }
 
   public String getName() {
@@ -19,6 +37,7 @@ public class Student {
 
   public void setName(String name) {
     // TODO set student's name
+    this.name = name;
   }
 
   public int getRating() {
@@ -27,24 +46,31 @@ public class Student {
 
   public void setRating(int rating) {
     // TODO initialize rating;
+    this.rating = rating;
+    sumOfRating += rating;
   }
 
   public boolean betterStudent(Student student) {
     // TODO return the result of comparing this.student's rating with the student's rating
-    return false;
+    return this.rating > student.rating;
   }
 
   public void changeRating(int rating) {
     // TODO change this student's rating and average rating of all students
+    sumOfRating -= this.rating;
+    this.rating = rating;
+    sumOfRating += rating;
   }
 
   public static void removeStudent(Student student) {
     // TODO remove student
+    sumOfRating = sumOfRating - student.rating;
+    count--;
   }
 
   @Override
   public String toString() {
     // TODO return String with name and rating of this student
-    return "";
+    return this.name + " " + this.rating;
   }
 }
